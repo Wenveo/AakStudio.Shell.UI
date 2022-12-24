004559fed335bc2a9e5c789ffa8e0dacdd9d3607
Noisrev <Noisrev@outlook.com>
2022-12-25 00:23:12 +0800

    Activates the content in the docking manager when the window is active

--- Aak.Shell.UI.Showcase/MainWindow.xaml.cs
+++ Aak.Shell.UI.Showcase/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Aak.Shell.UI.Controls;
 using Aak.Shell.UI.Showcase.ViewModels.Collection;
+using AvalonDock.Layout;
+using System.Linq;
 
 namespace Aak.Shell.UI.Showcase;
 
@@ -25,6 +27,42 @@ public sealed partial class MainWindow : MetroWindow
             mainStatusBar.ClearValue(ForegroundProperty);
 
             isCleanValue = true;
+        }
+    }
+
+    private void MetroWindow_Activated(object sender, System.EventArgs e)
+    {
+        // if the window is activated, then active the item in docking manager
+        var hasFloatingWindow = false;
+
+        var items = dockingManager.Layout.Descendents().OfType<LayoutContent>().ToList();
+        for (var i = 0; i < items.Count; i++)
+        {
+            var item = items[i];
+            if (item.IsFloating)
+            {
+                if (!hasFloatingWindow)
+                    hasFloatingWindow = true;
+                items.RemoveAt(i);
+            }
+        }
+
+        if (hasFloatingWindow && items.Count > 0)
+        {
+            var index = 0;
+
+            var tmpTimeStamp = items[0].LastActivationTimeStamp;
+            for (var j = 1; j < items.Count; j++)
+            {
+                var item2 = items[j];
+                if (item2.LastActivationTimeStamp > tmpTimeStamp)
+                {
+                    tmpTimeStamp = item2.LastActivationTimeStamp;
+                    index = j;
+                }
+            }
+
+            items[index].IsActive = true;
         }
     }
 }
