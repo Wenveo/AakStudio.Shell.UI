ce0907000e9d835947de738c06ce001743e1a1d9
Noisrev <Noisrev@outlook.com>
2022-12-24 23:39:22 +0800

    Add a floating document window style

---
+++ Aak.Shell.UI.Showcase/Converters/DocumentWellTitleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Aak.Shell.UI.Showcase.Converters;
+
+internal sealed class DocumentWellTitleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var mainWindowTitle = Application.Current.MainWindow.Title;
+        if (value is not null)
+        {
+            mainWindowTitle += $" - {value}";
+        }
+
+        return mainWindowTitle;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
