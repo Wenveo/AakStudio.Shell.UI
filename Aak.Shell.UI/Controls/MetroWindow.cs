afb591c3ad960fe39e2939722a90dbb6e28c8c6a
Noisrev <Noisrev@outlook.com>
2022-12-28 23:56:23 +0800

    Improved MetroWindow and add null check for style

--- Aak.Shell.UI/Controls/MetroWindow.cs
+++ Aak.Shell.UI/Controls/MetroWindow.cs
@@ -8,143 +8,157 @@ namespace Aak.Shell.UI.Controls
 {
     public class MetroWindow : Window
     {
-        public static bool IsWin11_Or_Latest => Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= 22000;
+        public static readonly DependencyProperty ActiveGlowBrushProperty =
+            DependencyProperty.Register(nameof(ActiveGlowBrush), typeof(SolidColorBrush),
+                typeof(MetroWindow), new PropertyMetadata(Brushes.Transparent));
 
-        public static readonly DependencyProperty ActiveGlowBrushProperty = DependencyProperty.Register(
-            "ActiveGlowBrush", typeof(SolidColorBrush), typeof(MetroWindow),
-            new PropertyMetadata(Brushes.Transparent));
-
-        public static readonly DependencyProperty InactiveGlowBrushProperty = DependencyProperty.Register(
-            "InactiveGlowBrush", typeof(SolidColorBrush), typeof(MetroWindow),
-            new PropertyMetadata(Brushes.Transparent));
+        public static readonly DependencyProperty InactiveGlowBrushProperty =
+            DependencyProperty.Register(nameof(InactiveGlowBrush), typeof(SolidColorBrush),
+                typeof(MetroWindow), new PropertyMetadata(Brushes.Transparent));
 
         /// <summary>
         ///     Left side of title bar
         /// </summary>
-        public static readonly DependencyProperty LeftWindowCommandsProperty = DependencyProperty.Register(
-            nameof(LeftWindowCommands),
-            typeof(FrameworkElement), typeof(MetroWindow));
+        public static readonly DependencyProperty LeftWindowCommandsProperty =
+            DependencyProperty.Register(nameof(LeftWindowCommands), typeof(FrameworkElement),
+                typeof(MetroWindow), new FrameworkPropertyMetadata(null));
 
         /// <summary>
         ///     Right side of title bar
         /// </summary>
-        public static readonly DependencyProperty RightWindowCommandsProperty = DependencyProperty.Register(
-            nameof(RightWindowCommands),
-            typeof(FrameworkElement), typeof(MetroWindow));
+        public static readonly DependencyProperty RightWindowCommandsProperty =
+            DependencyProperty.Register(nameof(RightWindowCommands), typeof(FrameworkElement),
+                typeof(MetroWindow), new FrameworkPropertyMetadata(null));
 
         /// <summary>
         ///     Sets whether Icon is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowIconProperty = DependencyProperty.Register(nameof(IsShowIcon),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowIconProperty =
+            DependencyProperty.Register(nameof(IsShowIcon), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether Title is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowTitleProperty = DependencyProperty.Register(nameof(IsShowTitle),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowTitleProperty =
+            DependencyProperty.Register(nameof(IsShowTitle), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether TitleBar is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowTitleBarProperty = DependencyProperty.Register(nameof(IsShowTitleBar),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowTitleBarProperty =
+            DependencyProperty.Register(nameof(IsShowTitleBar), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether MinimizeButton is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowMinimizeButtonProperty = DependencyProperty.Register(nameof(IsShowMinimizeButton),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowMinimizeButtonProperty =
+            DependencyProperty.Register(nameof(IsShowMinimizeButton), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether MaximizeButton is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowMaximizeButtonProperty = DependencyProperty.Register(nameof(IsShowMaximizeButton),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowMaximizeButtonProperty =
+            DependencyProperty.Register(nameof(IsShowMaximizeButton), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether CloseButton is displayed
         /// </summary>
-        public static readonly DependencyProperty IsShowCloseButtonProperty = DependencyProperty.Register(nameof(IsShowCloseButton),
-            typeof(bool), typeof(MetroWindow), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsShowCloseButtonProperty =
+            DependencyProperty.Register(nameof(IsShowCloseButton), typeof(bool),
+                typeof(MetroWindow), new PropertyMetadata(true));
 
         /// <summary>
         ///     Sets whether CloseButton is displayed
         /// </summary>
-        public static readonly DependencyProperty OnMaximizedPaddingProperty = DependencyProperty.Register(nameof(OnMaximizedPadding),
-            typeof(Thickness), typeof(MetroWindow));
-
+        public static readonly DependencyProperty OnMaximizedPaddingProperty =
+            DependencyProperty.Register(nameof(OnMaximizedPadding), typeof(Thickness),
+                typeof(MetroWindow), new FrameworkPropertyMetadata(
+                    Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= 22000 ?
+                    default(Thickness) : new Thickness(8)));
 
         public SolidColorBrush ActiveGlowBrush
         {
-            get => (SolidColorBrush)this.GetValue(ActiveGlowBrushProperty);
-            set => this.SetValue(ActiveGlowBrushProperty, value);
+            get => (SolidColorBrush)GetValue(ActiveGlowBrushProperty);
+            set => SetValue(ActiveGlowBrushProperty, value);
         }
 
         public SolidColorBrush InactiveGlowBrush
         {
-            get => (SolidColorBrush)this.GetValue(InactiveGlowBrushProperty);
-            set => this.SetValue(InactiveGlowBrushProperty, value);
+            get => (SolidColorBrush)GetValue(InactiveGlowBrushProperty);
+            set => SetValue(InactiveGlowBrushProperty, value);
         }
 
         public FrameworkElement LeftWindowCommands
         {
-            get => (FrameworkElement)this.GetValue(LeftWindowCommandsProperty);
-            set => this.SetValue(LeftWindowCommandsProperty, value);
+            get => (FrameworkElement)GetValue(LeftWindowCommandsProperty);
+            set => SetValue(LeftWindowCommandsProperty, value);
         }
 
         public FrameworkElement RightWindowCommands
         {
-            get => (FrameworkElement)this.GetValue(RightWindowCommandsProperty);
-            set => this.SetValue(RightWindowCommandsProperty, value);
+            get => (FrameworkElement)GetValue(RightWindowCommandsProperty);
+            set => SetValue(RightWindowCommandsProperty, value);
         }
 
         public bool IsShowIcon
         {
-            get => (bool)this.GetValue(IsShowIconProperty);
-            set => this.SetValue(IsShowIconProperty, value);
+            get => (bool)GetValue(IsShowIconProperty);
+            set => SetValue(IsShowIconProperty, value);
         }
 
         public bool IsShowTitle
         {
-            get => (bool)this.GetValue(IsShowTitleProperty);
-            set => this.SetValue(IsShowTitleProperty, value);
+            get => (bool)GetValue(IsShowTitleProperty);
+            set => SetValue(IsShowTitleProperty, value);
         }
 
         public bool IsShowTitleBar
         {
-            get => (bool)this.GetValue(IsShowTitleBarProperty);
-            set => this.SetValue(IsShowTitleBarProperty, value);
+            get => (bool)GetValue(IsShowTitleBarProperty);
+            set => SetValue(IsShowTitleBarProperty, value);
         }
 
         public bool IsShowMinimizeButton
         {
-            get => (bool)this.GetValue(IsShowMinimizeButtonProperty);
-            set => this.SetValue(IsShowMinimizeButtonProperty, value);
+            get => (bool)GetValue(IsShowMinimizeButtonProperty);
+            set => SetValue(IsShowMinimizeButtonProperty, value);
         }
 
         public bool IsShowMaximizeButton
         {
-            get => (bool)this.GetValue(IsShowMaximizeButtonProperty);
-            set => this.SetValue(IsShowMaximizeButtonProperty, value);
+            get => (bool)GetValue(IsShowMaximizeButtonProperty);
+            set => SetValue(IsShowMaximizeButtonProperty, value);
         }
 
         public bool IsShowCloseButton
         {
-            get => (bool)this.GetValue(IsShowCloseButtonProperty);
-            set => this.SetValue(IsShowCloseButtonProperty, value);
+            get => (bool)GetValue(IsShowCloseButtonProperty);
+            set => SetValue(IsShowCloseButtonProperty, value);
         }
 
         public Thickness OnMaximizedPadding
         {
-            get => (Thickness)this.GetValue(OnMaximizedPaddingProperty);
-            set => this.SetValue(OnMaximizedPaddingProperty, value);
+            get => (Thickness)GetValue(OnMaximizedPaddingProperty);
+            set => SetValue(OnMaximizedPaddingProperty, value);
         }
 
         static MetroWindow()
         {
-            OnMaximizedPaddingProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(IsWin11_Or_Latest ? new Thickness(0) : new Thickness(8)));
-            StyleProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(Application.Current.TryFindResource("MetroWindowBaseStyle")));
+            var resourceKey = "MetroWindowBaseStyle";
+            var windowStyle = Application.Current.TryFindResource(resourceKey);
+            if (windowStyle == null)
+            {
+                var resourceDictionary = (ResourceDictionary)Application.LoadComponent(
+                    new Uri("/Aak.Shell.UI;Component/Styles/MetroWindow.xaml", UriKind.Relative));
+
+                windowStyle = resourceDictionary[resourceKey];
+            }
+            StyleProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(windowStyle));
         }
 
         public MetroWindow()
@@ -169,8 +183,8 @@ namespace Aak.Shell.UI.Controls
         {
             base.OnApplyTemplate();
 
-            this.InitializeGlowWindowBehaviorEx();
-            this.InitializeWindowChromeEx();
+            InitializeGlowWindowBehaviorEx();
+            InitializeWindowChromeEx();
         }
     }
 }
