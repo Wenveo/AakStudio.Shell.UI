27037bc60b123941c091b575db72d6f880fb172d
Noisrev <Noisrev@outlook.com>
2023-01-09 19:13:03 +0800

    Update AakThemeToIsCheckedConverter

--- Aak.Shell.UI.Showcase/Converters/AakThemeToIsCheckedConverter.cs
+++ Aak.Shell.UI.Showcase/Converters/AakThemeToIsCheckedConverter.cs
@@ -1,16 +1,15 @@
-﻿using Aak.Shell.UI.Themes;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Windows.Data;
 
 namespace Aak.Shell.UI.Showcase.Converters
 {
-    [ValueConversion(typeof(Theme), typeof(bool))]
+    [ValueConversion(typeof(AakTheme), typeof(bool))]
     internal sealed class AakThemeToIsCheckedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is not Theme theme || parameter is not string str)
+            if (value is not AakTheme theme || parameter is not string str)
             {
                 return false;
             }
