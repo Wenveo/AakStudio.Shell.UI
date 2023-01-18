66e28b4bba31a1223987319af15f407fbc081e35
Noisrev <Noisrev@outlook.com>
2023-01-18 20:28:48 +0800

    Add `DataGridView` in showcase

---
+++ Aak.Shell.UI.Showcase/ControlViews/DataGridView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Aak.Shell.UI.Showcase.ControlViews
+{
+    /// <summary>
+    /// Interaction logic for DataGridView.xaml
+    /// </summary>
+    public partial class DataGridView : UserControl
+    {
+        public DataGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
