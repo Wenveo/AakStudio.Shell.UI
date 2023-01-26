d7b65b9281d856d7f7890d5f79309c096871c065
Noisrev <Noisrev@outlook.com>
2023-01-26 17:33:42 +0800

    Add comments for `WindowSystemButton`

--- AakStudio.Shell.UI/Controls/WindowSystemButton.cs
+++ AakStudio.Shell.UI/Controls/WindowSystemButton.cs
@@ -4,25 +4,49 @@ using System.Windows.Controls.Primitives;
 
 namespace AakStudio.Shell.UI.Controls
 {
+    /// <summary>
+    /// The types of window system button.
+    /// </summary>
     public enum WindowSystemButtonType
     {
+        /// <summary>
+        /// The minimize button.
+        /// </summary>
         Minimize,
+        /// <summary>
+        /// The maximize/restore button.
+        /// </summary>
         Maximize,
+        /// <summary>
+        /// The close button.
+        /// </summary>
         Close
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class WindowSystemButton : ButtonBase
     {
+        /// <summary>
+        /// Identifies the <see cref="WindowSystemButtonType"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty WindowSystemButtonTypeProperty =
             DependencyProperty.Register(nameof(WindowSystemButtonType), typeof(WindowSystemButtonType),
                 typeof(WindowSystemButton), new FrameworkPropertyMetadata(WindowSystemButtonType.Minimize));
 
+        /// <summary>
+        /// The button type of <see cref="WindowSystemButton"/>.
+        /// </summary>
         public WindowSystemButtonType WindowSystemButtonType
         {
             get => (WindowSystemButtonType)GetValue(WindowSystemButtonTypeProperty);
             set => SetValue(WindowSystemButtonTypeProperty, value);
         }
 
+        /// <summary>
+        /// The static class constructor of the <see cref="WindowSystemButton"/>.
+        /// </summary>
         static WindowSystemButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowSystemButton), new FrameworkPropertyMetadata(typeof(WindowSystemButton)));
